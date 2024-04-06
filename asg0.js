6a9c6883590b23f4a6923afe4197feb12fbc7389
Erik Chao <75496487+echao-03@users.noreply.github.com>
2024-04-06 14:49:17 -0700

    adding print statements to magnitude

--- asg0.js
+++ asg0.js
@@ -118,6 +118,8 @@ function handleDrawOperationEvent() {
     else if (selectedValue == "mag") {
         magVal1 = v1.magnitude();
         magVal2 = v2.magnitude();
+        console.log(magVal1);
+        console.log(magVal2);
     }
 
     else if (selectedValue == "norm") {
