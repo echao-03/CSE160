61025992b9076be1e2c41cbaea891df3486c37e2
Erik Chao <etchao@ucsc.edu>
2024-06-05 16:41:53 -0700

    Adding rain to repo

--- asg5/script.js
+++ asg5/script.js
@@ -73,14 +73,13 @@ camera.add(listener);
 // create a global audio source
 const sound = new THREE.Audio(listener);
 
-// load a sound and set it as the Audio object's buffer
-// const audioLoader = new THREE.AudioLoader();
-// audioLoader.load('sounds/rain.wav', function (buffer) {
-//     sound.setBuffer(buffer);
-//     sound.setLoop(true);
-//     sound.setVolume(0.5);
-//     sound.play();
-// });
+const audioLoader = new THREE.AudioLoader();
+audioLoader.load('sounds/rain.wav', function (buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.5);
+    sound.play();
+});
 
 const cubeWidth = 1;
 const cubeHeight = 1;
